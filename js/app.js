fd8362a51e5e115f01ebe5f4f21177069a1b4bb8
Danilo Cardoso <danilo.mecatronica2015@gmail.com>
2024-09-20 19:20:50 -0300

    Confirmação mostrando nome

--- js/app.js
+++ js/app.js
@@ -2,13 +2,16 @@ function alterarStatus(idGame) {
     
     let itemImage = document.getElementById(`game-${idGame}`).querySelector('div');
     let itemButton = document.getElementById(`game-${idGame}`).querySelector('a');
+    let itemName = document.getElementById(`game-${idGame}`).querySelector('p').textContent;
     
     if (itemImage.classList.contains('dashboard__item__img--rented')) {
-        if (confirm('Deseja mesmo devolver esse jogo?')) {;
+        
+        if (confirm(`Deseja mesmo devolver o jogo "${itemName}"?`)) {
             itemImage.classList.remove('dashboard__item__img--rented');
             itemButton.classList.remove('dashboard__item__button--return');
             itemButton.text = 'Alugar';
-        }    
+        }  
+          
     } else {
         itemImage.classList.add('dashboard__item__img--rented');
         itemButton.classList.add('dashboard__item__button--return');
